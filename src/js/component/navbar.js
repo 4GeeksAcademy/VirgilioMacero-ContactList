4da9d3b51f89f55ef0d9f97efeed99eacc7f64ab
virgilio macero <virgiliomacero@gmail.com>
2024-04-06 00:57:12 -0400

    Latest Modifications

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,22 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="container  mb-3">
+      <div className="navbar navbar-light ">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            Contact List from Virgilio
+          </span>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,48 +1,67 @@
 import React, { useState, useEffect } from "react";
-import getState from "./flux.js";
+// import getState from "./flux.js";
 
 // Don't change, here is where we initialize our context, by default it's just going to be null.
 export const Context = React.createContext(null);
 
 // This function injects the global store to any view/component where you want to use it, we will inject the context to layout.js, you can see it here:
 // https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.js#L35
-const injectContext = PassedComponent => {
-	const StoreWrapper = props => {
-		//this will be passed as the contenxt value
-		const [state, setState] = useState(
-			getState({
-				getStore: () => state.store,
-				getActions: () => state.actions,
-				setStore: updatedStore =>
-					setState({
-						store: Object.assign(state.store, updatedStore),
-						actions: { ...state.actions }
-					})
-			})
-		);
+const injectContext = (PassedComponent) => {
+  const StoreWrapper = (props) => {
+    //this will be passed as the contenxt value
+    // const [state, setState] = useState(
+    //   getState({
+    //     getStore: () => state.store,
+    //     getActions: () => state.actions,
+    //     setStore: (updatedStore) =>
+    //       setState({
+    //         store: Object.assign(state.store, updatedStore),
+    //         actions: { ...state.actions },
+    //       }),
+    //   })
+    // );
 
-		useEffect(() => {
-			/**
-			 * EDIT THIS!
-			 * This function is the equivalent to "window.onLoad", it only runs once on the entire application lifetime
-			 * you should do your ajax requests or fetch api requests here. Do not use setState() to save data in the
-			 * store, instead use actions, like this:
-			 *
-			 * state.actions.loadSomeData(); <---- calling this function from the flux.js actions
-			 *
-			 **/
-		}, []);
+    const [state, setState] = useState([]);
 
-		// The initial value for the context is not null anymore, but the current state of this component,
-		// the context will now have a getStore, getActions and setStore functions available, because they were declared
-		// on the state of this component
-		return (
-			<Context.Provider value={state}>
-				<PassedComponent {...props} />
-			</Context.Provider>
-		);
-	};
-	return StoreWrapper;
+    async function getContacts() {
+      const contacts = await fetch(
+        "https://playground.4geeks.com/contact/agendas/virgilios_agenda"
+      ).then(async (res) => {
+        if (res.status === 200) {
+          const query = await fetch(
+            "https://playground.4geeks.com/contact/agendas/virgilios_agenda/contacts"
+          );
+          const consult = await query.json();
+          setState(consult);
+        } else {
+          await fetch(
+            "https://playground.4geeks.com/contact/agendas/virgilios_agenda",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          getContacts();
+        }
+      });
+    }
+
+    useEffect(() => {
+      getContacts();
+    }, []);
+
+    // The initial value for the context is not null anymore, but the current state of this component,
+    // the context will now have a getStore, getActions and setStore functions available, because they were declared
+    // on the state of this component
+    return (
+      <Context.Provider value={state}>
+        <PassedComponent {...props} />
+      </Context.Provider>
+    );
+  };
+  return StoreWrapper;
 };
 
 export default injectContext;
